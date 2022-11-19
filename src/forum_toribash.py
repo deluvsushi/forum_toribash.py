efad319f6766833e71379f38de3dad9efa5eb233
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:45:28 +0800

    Update forum_toribash.py

--- src/forum_toribash.py
+++ src/forum_toribash.py
@@ -3,7 +3,7 @@ from hashlib import md5
 from html_to_json import convert
 
 class ForumToribash:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://forum.toribash.com"
 		self.recaptcha_api = "https://www.google.com/recaptcha/api2"
 		self.user_id = None
@@ -15,7 +15,7 @@ class ForumToribash:
 			"cookie": f"tb5sessionhash={self.tb5_session_hash}; tb5userid={self.user_id}; tb5password={self.tb5_password}"
 		}
 	
-	def md5hash(self, string: str):
+	def md5_hash(self, string: str) -> str:
 		return md5(string.encode()).hexdigest()
 
 	def generate_captcha(
@@ -27,7 +27,7 @@ class ForumToribash:
 			v: str = "PRMRaAwB3KlylGQR57Dyk-pF",
 			key: str = "6LfTeLkUAAAAAFXarNTDL29B5TJoMmvtADlvilA7",
 			co: str = "aHR0cHM6Ly9mb3J1bS50b3JpYmFzaC5jb206NDQz",
-			bg: str = "!vbugu74KAAQVHCQMbQEHDwJliyi_1YCin_bisYSgvOK-_oSWYd-CVRtB_IoKv1xL4XhGFCUVFbA8AdYR8zp8H0o87SEuvBRoEZqbHG51z2qcDbRQtJz-gSWU0FqJC7PbYnMaPu7V3guQWSx7MnutKGkcR6AGPE6-oNh5e448w67CDud6EnmyeeCxfK-4sLDl8kXBgnZQefz-RsnicYe3gaD8gGmhq7rFPHiw78zA1Qkl9_H4i1tLgzapnluB_grL8yzpGMLXx3Wh37E0OmjWivHZlmQIxnKm9sRU-IYhQbFkwyi0jDBsX9Etn_YKb0mnrHQSg770XyEyIpSW7RCjWR-p1AjA-g_iL5QI7QeMuL4WmNyYhrN4BsQR5hLD5kPUyno3J5dvfYjxOiEiESNF81oyK8gWZZJ1kEcc2zJ0UtjBR4F0jmbauD5ZII2Z1UbNVooGTe4ydmUNHfUCbqmJ1HRirPuqCXDU868gi9deYB83grzG1c_S9KbUt74zilNn6See0MQs2qXIDGZzZQUwkLE5Xg617COHODV14GNc22eg_Ctj1jlE9SbOcUhdVVKy1-AbH-gpCD9BzsbmLibCQl6uaMYthuqkqLCDDjvmjpo-iIcPNauq29djP6muvQoPqQ1HNBMdV0Y5P64TrfGLenU8zirLIM2k0XnbfoY_iTDyVw763qDhKGy-I3O-k8-VPGTXKWyS5wah9brnPQ9nKPjufsBvxIyDjzUYsK1I0dZfiUoVYTconHc-OH7yXQxjlWseGkrnYjLqjFZIInwVbj0dWozXVhE8-1kLyFDpMdDDY4da1oSAGq2oJKbFDSrqzLJPf98DQJwAT9jTM0rdk9i9ZrIaLP2Eihx2CNJw8SUVe8yHp0mb13Kel3-oLqtMYay2iO2P8XYQplvH1165et2NBgGkRUCTpMd7l-9979U8w93TjUGoxok*"):
+			bg: str = "!vbugu74KAAQVHCQMbQEHDwJliyi_1YCin_bisYSgvOK-_oSWYd-CVRtB_IoKv1xL4XhGFCUVFbA8AdYR8zp8H0o87SEuvBRoEZqbHG51z2qcDbRQtJz-gSWU0FqJC7PbYnMaPu7V3guQWSx7MnutKGkcR6AGPE6-oNh5e448w67CDud6EnmyeeCxfK-4sLDl8kXBgnZQefz-RsnicYe3gaD8gGmhq7rFPHiw78zA1Qkl9_H4i1tLgzapnluB_grL8yzpGMLXx3Wh37E0OmjWivHZlmQIxnKm9sRU-IYhQbFkwyi0jDBsX9Etn_YKb0mnrHQSg770XyEyIpSW7RCjWR-p1AjA-g_iL5QI7QeMuL4WmNyYhrN4BsQR5hLD5kPUyno3J5dvfYjxOiEiESNF81oyK8gWZZJ1kEcc2zJ0UtjBR4F0jmbauD5ZII2Z1UbNVooGTe4ydmUNHfUCbqmJ1HRirPuqCXDU868gi9deYB83grzG1c_S9KbUt74zilNn6See0MQs2qXIDGZzZQUwkLE5Xg617COHODV14GNc22eg_Ctj1jlE9SbOcUhdVVKy1-AbH-gpCD9BzsbmLibCQl6uaMYthuqkqLCDDjvmjpo-iIcPNauq29djP6muvQoPqQ1HNBMdV0Y5P64TrfGLenU8zirLIM2k0XnbfoY_iTDyVw763qDhKGy-I3O-k8-VPGTXKWyS5wah9brnPQ9nKPjufsBvxIyDjzUYsK1I0dZfiUoVYTconHc-OH7yXQxjlWseGkrnYjLqjFZIInwVbj0dWozXVhE8-1kLyFDpMdDDY4da1oSAGq2oJKbFDSrqzLJPf98DQJwAT9jTM0rdk9i9ZrIaLP2Eihx2CNJw8SUVe8yHp0mb13Kel3-oLqtMYay2iO2P8XYQplvH1165et2NBgGkRUCTpMd7l-9979U8w93TjUGoxok*") -> str:
 		parameters = f"v={v}&reason=q&c=<token>&k={key}&co={co}&hl={hl}&size=invisible&chr={chr}&vh={vh}&bg={bg}"
 		anchor = requests.get(
 			f"{self.recaptcha_api}/anchor?ar=1&k={key}&co={co}&hl={hl}&v={v}&size=invisible&cb={cb}",
@@ -49,7 +49,7 @@ class ForumToribash:
 			email: str,
 			username: str,
 			password: str,
-			tos: str = "on"):
+			tos: str = "on") -> dict:
 		data = {
 			"email": email,
 			"username": username,
@@ -65,12 +65,12 @@ class ForumToribash:
 				data=data,
 				headers=self.headers).text)
 
-	def login(self, username: str, password: str):
+	def login(self, username: str, password: str) -> dict:
 		data = {
 			"vb_login_username": username,
 			"vb_login_password": password,
-			"vb_login_md5password": self.md5hash(password),
-			"vb_login_md5password_utf": self.md5hash(password),
+			"vb_login_md5password": self.md5_hash(password),
+			"vb_login_md5password_utf": self.md5_hash(password),
 			"cookieuser": 1,
 			"do": "login",
 			"securitytoken": "guest",
@@ -89,49 +89,49 @@ class ForumToribash:
 			self.headers["cookie"] = f"tb5sessionhash={self.tb5_session_hash}; tb5userid={self.user_id}; tb5password={self.tb5_password}"
 		return text
 
-	def get_user_info(self, user_id: int):
+	def get_user_info(self, user_id: int) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/member.php?u={user_id}",
 				headers=self.headers).text)
 
-	def get_ranking(self):
+	def get_ranking(self) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/tori_ranking.php",
 				headers=self.headers).text)
 
-	def get_clans(self):
+	def get_clans(self) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/clans.php",
 				headers=self.headers).text)
 
-	def get_market(self):
+	def get_market(self) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/marketplace.php",
 				headers=self.headers).text)
 
-	def get_market_item(self, item_id: int):
+	def get_market_item(self, item_id: int) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/marketplace.php?itemid={item_id}",
 				headers=self.headers).text)
 
-	def get_all_items(self):
+	def get_all_items(self) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/tori_shop.php",
 				headers=self.headers).text)
 
-	def get_user_inventory(self, user_id: int):
+	def get_user_inventory(self, user_id: int) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/tori_inventory.php?u={user_id}",
 				headers=self.headers).text)
 
-	def get_toribash_staff(self):
+	def get_toribash_staff(self) -> dict:
 		return convert(
 			requests.get(
 				f"{self.api}/showgroups.php",
